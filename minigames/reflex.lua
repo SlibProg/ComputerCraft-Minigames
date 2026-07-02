54ec1cef0ee67c4d92e17d0af8d3d2d1a6f0b3f4
SlibProg <enzomtg86@gmail.com>
2026-07-02 10:43:16 -0300

    Added Reflex

--- minigames/reflex.lua
+++ minigames/reflex.lua
@@ -1,37 +1,50 @@
 term.clear()
 term.setCursorPos(1, 1)
-
 math.randomseed(os.time())
 
 local time = math.random(2, 15)
 local letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
     "v", "w", "x", "y", "z" }
-
-local letterDecider = math.random(1, #letters)
-local letter = letters[letterDecider]
+local letter = letters[math.random(1, #letters)]
 
 sleep(time)
-local timer = os.startTimer(20)
-term.write("Type " .. letter ..)
-local input = read()
 
-if timer == 0 then
-    if input == letter then
-        term.write("Times Up! You typed in 20 seconds!")
-        sleep(3)
-        term.clear()
-        term.setCursorPos(1, 1)
-    else
-        term.write("Times Up! You... didn't type in time!")
-        sleep(3)
-        term.clear()
-        term.setCursorPos(1, 1)
+term.write("Type " .. letter .. ": ")
+
+local startClock = os.clock()
+local timerId = os.startTimer(20)
+local input = nil
+local timedOut = false
+
+-- roda leitura e timer em paralelo
+parallel.waitForAny(
+    function()
+        input = read()
+    end,
+    function()
+        while true do
+            local event, p1 = os.pullEvent("timer")
+            if p1 == timerId then
+                timedOut = true
+                break
+            end
+        end
     end
+)
+
+local elapsed = os.clock() - startClock
+
+term.clear()
+term.setCursorPos(1, 1)
+
+if timedOut then
+    print("Time's up! You didn't type in time!")
+elseif input == letter then
+    print("Correct! You typed in " .. string.format("%.1f", elapsed) .. " seconds!")
 else
-    if input == letter then
-        term.write("Times Up! You typed in " .. timer .. " seconds!")
-        sleep(3)
-        term.clear()
-        term.setCursorPos(1, 1)
-    end
+    print("Wrong letter! You typed '" .. tostring(input) .. "' instead of '" .. letter .. "'.")
 end
+
+sleep(3)
+term.clear()
+term.setCursorPos(1, 1)
